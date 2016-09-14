1024f198a1ae63163c22abf3f45ab8064231a43e
Lawrence Liu <lawrence255@yahoo.com>
2016-09-14 14:05:28 -0400

    bleh

--- Names.py
+++ Names.py
@@ -1,20 +1,29 @@
-def Name (period):
-    pd4 = ['Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson', 'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin', 'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth', 'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj', 'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward', 'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina']
-    
-    pd8 = ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki', 'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda', 'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vincent', 'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel', 'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio', 'Kelly', 'William', 'Jordan', 'Haley', 'Henry']
-    
-    pd9 = ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C', 'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha', 'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry', 'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine', 'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L', 'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+   CLASSES =  {
+    4: [ 'Ayman', 'Shaeq', 'Patrick', 'Yvonne', 'Wilson',
+         'Brian', 'Farhan', 'Janet', 'Harry', 'Kevin',
+         'Nicholas', 'Jason', 'Yikai', 'Emma', 'Kenneth',
+         'Nala', 'Karol', 'Elias', 'Ely', 'Reo', 'Dhriaj',
+         'Amy', 'Arvind', 'Nobel', 'Angela', 'Edward',
+         'Jonathan', 'Celine', 'Daniel', 'Lindsey', 'Ziyan', 'Elina'],
+    8: ['Julian', 'Sebastian', 'Jordan', 'Alan', 'Yuki',
+        'Chloe', 'Noah', 'Edvic', 'Jia Qi', 'Shan', 'Rodda',
+        'Anya', 'Edmond', 'Asher', 'Kathy', 'Sharon', 'Vncent',
+        'Lawrence', 'Sachal', 'Gabriel', 'Jason', 'Daniel',
+        'Felix', 'Jacob', 'Bayle', 'Fortune', 'Gio',
+        'Kelly', 'William', 'Jordan', 'Haley', 'Henry'],
+    9: ['James', 'Vanna', 'Zicheng', 'Quinn', 'Anthony C',
+        'Joel', 'Steph', 'Xinhui', 'Richard', 'Misha',
+        'Maddie', 'Winston', 'Shariar', 'Nancy', 'Jerry',
+        'Michael', 'Stiven', 'Will',  'Olivia', 'Constantine',
+        'Jessica', 'Kate', 'Shamaul', 'Max', 'Sarah', 'Anthony L',
+        'Brandon', 'Nicole', 'Brian', 'Issac', 'Seiji', 'Lorenz']
+}
 
-    if (period == 4):
-        return pd4[len(pd4)*random()]
-    if (period == 8):
-        return pd8[randint(0,len(pd8)-1)]
-    if (period == 9):
-        return pd9[randint(0,len(pd9)-1)]
-
-    Name (4)
-    Name (8)
-    Name (9)
-    Name (4)
-    Name (8)
-    Name (9)
+       
+        
+Name (4)
+Name (8)
+Name (9)
+Name (4)
+Name (8)
+Name (9)
